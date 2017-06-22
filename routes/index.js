2070ffe782665145b5c13560490022ef51081e82
条件状语从句 <lixzap@gmail.com>
2017-06-22 22:10:06 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '李响' });
 });
 
 module.exports = router;
